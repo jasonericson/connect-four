29c1b9fdc389a0074fe4d31a535bfe3d0b2cb5af
Jason Ericson <jasonmarkericson@gmail.com>
2025-07-24 18:59:20 +0000

    Bit stack

--- main.cpp
+++ main.cpp
@@ -17,11 +17,7 @@
     label##_time_spent += (SDL_GetPerformanceCounter() - label##_time_start);
 #define PRINT_PROFILE_INFO(label) print_profile_info(#label, label##_count, label##_time_spent)
 
-struct BoardState
-{
-    uint64_t player1;
-    uint64_t player2;
-};
+typedef uint8_t BoardState[7];
 
 inline uint8_t bit_check(uint64_t num, uint8_t pos)
 {
@@ -33,40 +29,65 @@ inline uint64_t bit_set(uint64_t num, uint8_t pos)
     return num | ((uint64_t)1 << pos);
 }
 
-uint8_t get_board_value(BoardState* state, uint8_t row, uint8_t col)
+uint8_t get_board_value(BoardState state, uint8_t row, uint8_t col)
 {
     assert(row < 6 && col < 7);
 
-    uint8_t bit_pos = (row * 7) + col;
+    return state[col] < (1 << (row + 1)) ? 0 : ((state[col] & (1 << row)) >> row) + 1;
 
-    uint8_t result = bit_check(state->player1, bit_pos) + bit_check(state->player2, bit_pos) * 2;
-    assert(result >= 0 && result < 3);
+    // uint8_t bit_pos = (row * 7) + col;
 
-    // return bit_check(state->player1, bit_pos) ? 1 : (bit_check(state->player2, bit_pos) ? 2 : 0);
-    return bit_check(state->player1, bit_pos) + bit_check(state->player2, bit_pos) * 2;
+    // uint8_t result = bit_check(state->player1, bit_pos) + bit_check(state->player2, bit_pos) * 2;
+    // assert(result >= 0 && result < 3);
+
+    // // return bit_check(state->player1, bit_pos) ? 1 : (bit_check(state->player2, bit_pos) ? 2 : 0);
+    // return bit_check(state->player1, bit_pos) + bit_check(state->player2, bit_pos) * 2;
 }
 
-void set_board_value(BoardState* state, uint8_t row, uint8_t col, uint8_t value)
+bool make_move(uint8_t player, uint8_t column, BoardState state)
 {
-    assert(row < 6 && col < 7);
-    assert(value == 1 || value == 2);
+    assert(column >= 0 && column < 7);
 
-    uint8_t bit_pos = (row * 7) + col;
+    if (state[column] >= 0b01000000)
+        return false;
 
-    if (value == 1)
+    uint8_t probe = 0b01000000;
+    while (state[column] < probe)
     {
-        uint64_t player1 = bit_set(state->player1, bit_pos);
-        *state = { .player1 = player1, .player2 = state->player2 };
+        probe >>= 1;
     }
-    else
+
+    if (player == 1)
     {
-        uint64_t player2 = bit_set(state->player2, bit_pos);
-        *state = { .player1 = state->player1, .player2 = player2 };
+        state[column] &= ~(probe);
     }
+
+    state[column] |= (probe << 1);
+
+    return true;
 }
 
-bool check_for_win(BoardState* state)
+bool check_for_win(BoardState state)
 {
+    // Vertical
+    for (uint8_t col = 0; col < 7; ++col)
+    {
+        switch (state[col])
+        {
+            case 0b00010000:
+            case 0b00011111:
+            case 0b00100001:
+            case 0b00111110:
+            case 0b01000010:
+            case 0b01000011:
+            case 0b01111100:
+            case 0b01111101:
+                return true;
+            default:
+                break;
+        }
+    }
+
     // Horizontal
     for (uint8_t row = 0; row < 6; ++row)
     {
@@ -89,41 +110,6 @@ bool check_for_win(BoardState* state)
         }
 
         for (uint8_t col = 4; col < 7; ++col)
-        {
-            if (get_board_value(state, row, col) != center)
-                break;
-
-            ++count;
-
-            if (count == 4)
-            {
-                return true;
-            }
-        }
-    }
-
-    // Vertical
-    for (uint8_t col = 0; col < 7; ++col)
-    {
-        uint8_t center = get_board_value(state, 2, col);
-        if (center == 0 || center != get_board_value(state, 3, col))
-            continue;
-
-        uint8_t count = 2;
-        for (int8_t row = 1; row >= 0; --row)
-        {
-            if (get_board_value(state, row, col) != center)
-                break;
-
-            ++count;
-        }
-
-        if (count == 4)
-        {
-            return true;
-        }
-
-        for (uint8_t row = 4; row < 6; ++row)
         {
             if (get_board_value(state, row, col) != center)
                 break;
@@ -218,28 +204,6 @@ bool check_for_win(BoardState* state)
     return false;
 }
 
-bool make_move(uint8_t player, uint8_t column, BoardState* state)
-{
-    assert(column >= 0 && column < 7);
-
-    if (get_board_value(state, 5, column) != 0)
-    {
-        return false;
-    }
-
-    for (uint8_t row = 0; row < 6; ++row)
-    {
-        if (get_board_value(state, row, column) == 0)
-        {
-            set_board_value(state, row, column, player);
-
-            return true;
-        }
-    }
-
-    return false;
-}
-
 uint64_t total_moves_evaluated = 0;
 uint64_t wins_found = 0;
 uint64_t dead_ends_found = 0;
@@ -260,7 +224,7 @@ void print_profile_info(const char* label, uint64_t count, uint64_t time_spent)
         label, count, total_time_seconds, total_time_percentage, avg_time_nanoseconds);
 }
 
-int32_t get_move_score(uint8_t player, uint8_t player_this_turn, uint8_t col, BoardState* state, bool *move_possible, uint8_t depth)
+int32_t get_move_score(uint8_t player, uint8_t player_this_turn, uint8_t col, BoardState state, bool *move_possible, uint8_t depth)
 {
     uint64_t time_current = SDL_GetPerformanceCounter();
     double_t seconds_since_last_print = (double_t)(time_current - time_of_last_print) / (double_t)SDL_GetPerformanceFrequency();
@@ -280,9 +244,10 @@ int32_t get_move_score(uint8_t player, uint8_t player_this_turn, uint8_t col, Bo
 
     START_PROFILE(get_move_score)
 
-    BoardState state_new = *state;
+    BoardState state_new;
+    memcpy(state_new, state, sizeof(BoardState));
     START_PROFILE(make_move)
-    *move_possible = make_move(player_this_turn, col, &state_new);
+    *move_possible = make_move(player_this_turn, col, state_new);
     END_PROFILE(make_move)
     if (*move_possible == false)
     {
@@ -296,7 +261,7 @@ int32_t get_move_score(uint8_t player, uint8_t player_this_turn, uint8_t col, Bo
     ++total_moves_evaluated;
 
     START_PROFILE(check_for_win)
-    bool result = check_for_win(&state_new);
+    bool result = check_for_win(state_new);
     END_PROFILE(check_for_win)
     if (result)
     {
@@ -314,7 +279,7 @@ int32_t get_move_score(uint8_t player, uint8_t player_this_turn, uint8_t col, Bo
     for (uint8_t col = 0; col < 7; ++col)
     {
         bool temp_move_possible;
-        total_score += get_move_score(player, player_this_turn, col, &state_new, &temp_move_possible, depth + 1);
+        total_score += get_move_score(player, player_this_turn, col, state_new, &temp_move_possible, depth + 1);
     }
 
     min_depth = depth < min_depth ? depth : min_depth;
@@ -322,11 +287,94 @@ int32_t get_move_score(uint8_t player, uint8_t player_this_turn, uint8_t col, Bo
     return total_score;
 }
 
+void sprint_board(BoardState state, char* buffer)
+{
+    sprintf(buffer, "_1_2_3_4_5_6_7_\n");
+    buffer += 16;
+    for (int8_t row = 6 - 1; row >= 0; --row)
+    {
+        sprintf(buffer, "|");
+        buffer += 1;
+        for (uint8_t col = 0; col < 7; ++col)
+        {
+            switch (get_board_value(state, row, col))
+            {
+                case 1:
+                    sprintf(buffer, "X");
+                    buffer += 1;
+                    break;
+                case 2:
+                    sprintf(buffer, "O");
+                    buffer += 1;
+                    break;
+                default:
+                    sprintf(buffer, " ");
+                    buffer += 1;
+                    break;
+            }
+
+            sprintf(buffer, "|");
+            buffer += 1;
+        }
+        sprintf(buffer, "\n");
+        buffer += 1;
+    }
+    sprintf(buffer, "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
+}
+
+void print_board(BoardState state)
+{
+    char* board = (char*)malloc(sizeof(char) * 256);
+    sprint_board(state, board);
+    printf("%s", board);
+}
+
+bool test_print_board()
+{
+    BoardState state = { 1, 1, 1, 1, 1, 1, 1 };
+
+    make_move(1, 5, state);
+    make_move(2, 0, state);
+    make_move(1, 6, state);
+    make_move(2, 6, state);
+    make_move(1, 0, state);
+    make_move(2, 3, state);
+    make_move(1, 4, state);
+    make_move(2, 1, state);
+    make_move(1, 3, state);
+    make_move(2, 4, state);
+    make_move(1, 5, state);
+    make_move(2, 5, state);
+    make_move(1, 4, state);
+    make_move(2, 5, state);
+    make_move(1, 3, state);
+    make_move(2, 0, state);
+    make_move(1, 1, state);
+    make_move(2, 1, state);
+    
+    const char* correct_board =
+        "_1_2_3_4_5_6_7_\n"
+        "| | | | | | | |\n"
+        "| | | | | | | |\n"
+        "| | | | | |O| |\n"
+        "|O|O| |X|X|O| |\n"
+        "|X|X| |X|O|X|O|\n"
+        "|O|O| |O|X|X|X|\n"
+        "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n";
+
+    char* test_board = (char*)malloc(sizeof(char) * 256);
+    sprint_board(state, test_board);
+
+    return strcmp(correct_board, test_board) == 0;
+}
+
 int main()
 {
     SDL_Init(0);
 
-    BoardState state = { .player1 = 0, .player2 = 0 };
+    assert(test_print_board());
+
+    BoardState state = { 1, 1, 1, 1, 1, 1, 1 };
     uint8_t player = 1;
 
     // for (uint8_t row = 0; row < 6; ++row)
@@ -339,31 +387,31 @@ int main()
 
     while (true)
     {
-        // Print board
-        printf("_1_2_3_4_5_6_7_\n");
-        for (int8_t row = 6 - 1; row >= 0; --row)
-        {
-            printf("|");
-            for (uint8_t col = 0; col < 7; ++col)
-            {
-                switch (get_board_value(&state, row, col))
-                {
-                    case 1:
-                        printf("X");
-                        break;
-                    case 2:
-                        printf("O");
-                        break;
-                    default:
-                        printf(" ");
-                        break;
-                }
+        // // Print board
+        // printf("_1_2_3_4_5_6_7_\n");
+        // for (int8_t row = 6 - 1; row >= 0; --row)
+        // {
+        //     printf("|");
+        //     for (uint8_t col = 0; col < 7; ++col)
+        //     {
+        //         switch (get_board_value(state, row, col))
+        //         {
+        //             case 1:
+        //                 printf("X");
+        //                 break;
+        //             case 2:
+        //                 printf("O");
+        //                 break;
+        //             default:
+        //                 printf(" ");
+        //                 break;
+        //         }
 
-                printf("|");
-            }
-            printf("\n");
-        }
-        printf("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
+        //         printf("|");
+        //     }
+        //     printf("\n");
+        // }
+        // printf("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
 
         // // Check board for wins
         // bool win = check_for_win(&state);
@@ -379,7 +427,7 @@ int main()
         bool move_possible;
         uint64_t eval_start_time = SDL_GetPerformanceCounter();
         time_of_last_print = eval_start_time;
-        int64_t move_score = get_move_score(player, player, 0, &state, &move_possible, 1);
+        int64_t move_score = get_move_score(player, player, 0, state, &move_possible, 1);
         // uint64_t eval_end_time = SDL_GetPerformanceCounter();
         // printf("Time to finish evaluation: %ld\n", (eval_end_time - eval_start_time) / 1000);
         printf("Column 0 move score: %ld\n", move_score);
